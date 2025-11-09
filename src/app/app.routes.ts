281500cda1b8a80100a7b819ddc37b57128b8c36
Basma Elmahany <basmakhalaf974@gmail.com>
2025-11-09 12:23:00 +0200

    error edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './Home/Components/home/home.component';
 export const routes: Routes = [
   { path: 'login', loadComponent: () => import('./Auth/Components/login/login.component').then(m => m.LoginComponent) },
   { path: 'register', loadComponent: () => import('./Auth/Components/register/register.component').then(m => m.RegisterComponent) },
-  { path: '', redirectTo: 'login', pathMatch: 'full' } // optional default
+  { path: '', redirectTo: 'login', pathMatch: 'full' } ,// optional default
   {
     path: '',
     component: HomeComponent, // ✅ layout ثابت
