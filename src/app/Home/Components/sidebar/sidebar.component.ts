3cbbae0d2e3547bf274298b79a9d849ea9ca7b67
Madoona-eng <madonnaessam2002@gmail.com>
2025-11-10 13:18:27 +0200

    done

--- src/app/Home/Components/sidebar/sidebar.component.ts
+++ src/app/Home/Components/sidebar/sidebar.component.ts
@@ -3,6 +3,9 @@ import { NavItem } from '../../Models/home';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslatePipe } from '../../../Shared/Pipes/translate.pipe';
+import { I18nService } from '../../../Shared/Services/i18n.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,12 +15,31 @@ import { TranslatePipe } from '../../../Shared/Pipes/translate.pipe';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
   @Input() isOpen = true;
   @Input() navItems: NavItem[] = [];
   @Output() languageToggle = new EventEmitter<void>();
+  @Output() sidebarToggle = new EventEmitter<void>();
+
+  currentLang$: Observable<string>;
+  isRTL$: Observable<boolean>;
+
+  constructor(private i18nService: I18nService) {
+    this.currentLang$ = this.i18nService.currentLang$;
+    this.isRTL$ = this.i18nService.currentLang$.pipe(
+      map(lang => lang === 'ar')
+    );
+  }
 
   toggleLanguage(): void {
     this.languageToggle.emit();
+  }
+
+  toggleSidebar(): void {
+    this.sidebarToggle.emit();
+  }
+
+  logout(): void {
+    // Implement logout logic here
+    console.log('Logging out...');
   }
 }
--- src/app/Project/Components/update/update.component.ts
+++ src/app/Project/Components/update/update.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AsyncPipe } from '@angular/common';
+import { TranslatePipe } from '../../../Shared/Pipes/translate.pipe';
 
 @Component({
   selector: 'app-update',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, TranslatePipe, AsyncPipe],
   templateUrl: './update.component.html',
-  styleUrl: './update.component.scss'
+  styleUrls: ['./update.component.scss']
 })
 export class UpdateComponent {
+  @Output() close = new EventEmitter<void>();
+  projectForm: FormGroup;
+  isEditMode = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<UpdateComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.isEditMode = !!data;
+    this.projectForm = this.fb.group({
+      name: [data?.name || '', Validators.required],
+      description: [data?.description || '']
+    });
+  }
+
+  save() {
+    if (this.projectForm.valid) {
+      const projectData = this.projectForm.value;
+      console.log(this.isEditMode ? 'Updating project:' : 'Saving project:', projectData);
+      this.dialogRef.close(projectData);
+    }
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
 }
