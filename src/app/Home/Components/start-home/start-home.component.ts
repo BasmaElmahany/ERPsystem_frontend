410f71525bdfa8d483c802454115b7efec3ab3c1
Madoona-eng <madonnaessam2002@gmail.com>
2025-11-24 14:32:34 +0200

    doneeeee

--- src/app/Home/Components/start-home/start-home.component.ts
+++ src/app/Home/Components/start-home/start-home.component.ts
@@ -716,11 +716,7 @@ export class StartHomeComponent implements AfterViewInit, OnDestroy {
         // navigate to main dashboard (projects) — adjusts to your routing default
         this.router.navigate(['/projects']);
     }
-    toggleLanguage(): void {
-        console.log("button click")
-    this.languageToggle.emit();
-  }
-   gotoDashboard(): void {
+    gotoDashboard(): void {
         // navigate to the project dashboard route
         this.router.navigate(['/dashboard']);
     }
